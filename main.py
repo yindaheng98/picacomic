58c1ffddea8cb94f3c98fbf94303679246fcfb7c
yindaheng98 <yindaheng98@163.com>
2021-07-14 11:45:45 +0800

    指定下载目录位置

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from picaact import PicaAction
 import optparse
+import os
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
@@ -7,6 +8,8 @@ if __name__ == "__main__":
                       type='string', help='pica用户名')
     parser.add_option('-p', '--pass', dest='pasw',
                       type='string', help='pica密码')
+    parser.add_option('-d', '--directory', dest='directory', default=os.path.join(os.path.split(__file__)[0], "data"),
+                      type='string', help='数据存放位置')
     parser.add_option('-x', '--exec', dest='exec', default='download',
                       type='string', help='执行指令: init 初始化数据库, update 更新数据库, download 下载图片，reset_download 重置下载记录（不会删除文件）')
     parser.add_option('-n', '--numb', dest='numb', default=0,
@@ -22,7 +25,9 @@ if __name__ == "__main__":
                    {
                        'http': options.proxy,
                        'https': options.proxy
-                   }, threadn=options.thread)
+                   },
+                   data_path=options.directory,
+                   threadn=options.thread)
 
     if options.exec == "init":
         if options.numb>0:
